cfe757b8bade70c0592f3fd7860137a2f7bb32b2
Deltafortal <aldolucchetta21@gmail.com>
2023-11-25 10:16:37 +0100

    Added basic header

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -23,17 +23,18 @@
 
         <!-- Title -->
         <div class="title">
-            <h1>AldoLucchetta</h1>
+            <i class="bi bi-list d-md-none"></i>
+            <h3>AldoLucchetta</h3>
         </div>
 
 
         <!-- Navbar -->
-        <div class="navbar">
+        <div class="navbar d-md-block">
             <ul>
-                <li>Home</li>
-                <li>Skills</li>
-                <li>Projects</li>
-                <li>Contacts</li>
+                <li><a href="">Home</a></li>
+                <li><a href="">Skills</a></li>
+                <li><a href="">Projects</a></li>
+                <li><a href="">Contacts</a></li>
             </ul>
         </div>
 
@@ -48,7 +49,55 @@
 
     @use '../assets/scss/vars' as *;
 
+    header {
 
+        padding: 20px;
+
+        display: flex;
+        align-items: center;
+
+        .title {
+            display: flex;
+            align-items: center;
+        }
+
+        h3 {
+            margin-left: 20px;
+        }
+
+        .navbar {
+            margin: 0 auto;
+
+            ul {
+                display: flex;
+                align-items: center;
+                justify-content: center;
+            }
+
+            li {
+                list-style-type: none;
+                margin-right: 15px;
+            }
+
+            a {
+                text-decoration: none;
+                color: black;
+                font-size: 18px;
+            }
+
+        }
+
+    }
     
+
+    .navbar {
+        display: none;
+    }
+
+
+    i {
+        font-size: 30px;
+    }
+
 
 </style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 import { createApp } from 'vue'
 import 'bootstrap/dist/css/bootstrap.css';
+import './assets/scss/style.scss';
+import 'bootstrap-icons/font/bootstrap-icons.css';
 
 import App from './App.vue'
 
